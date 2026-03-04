51a333007d046ac78679fb3f78eafd257c118789
mtong@cognitiv.ai <mtong@Michaels-MacBook-Pro.local>
2026-03-04 09:47:52 -0800

    works but is slow

--- src/constraints.py
+++ src/constraints.py
@@ -4,82 +4,129 @@ from typing import Optional
 
 import numpy as np
 
-
 class Constraint(ABC):
+    """
+    Constraints for the wires
+    """
+    EMPTY = -1
+
     def __init__(self):
         pass
 
     @abstractmethod
-    def get_positions(self)-> set[tuple[int, int]]:
-        """
-        Get all (player index, wire_position) tuples
-        for which this constraint is relevant for
-        """
+    def is_valid(
+            self,
+            wire_rank: int,
+            wire_limits_per_player: np.array,
+            remaining_wires: tuple[int, ...],
+            wire_distribution: tuple[int, ...],
+            is_terminal: bool,
+    ) -> bool:
         pass
 
-    @abstractmethod
-    def get_wires(self)-> set[int]:
-        """
-        Get all wire values for which this constraint is relevant for
-        """
-        pass
+    def mutate_constraint(
+            self,
+            wire_rank: int,
+            wire_limits_per_player: np.array,
+            remaining_wires: tuple[int, ...],
+            wire_distribution: tuple[int, ...],
+            is_terminal: bool,
+    ):
+        raise RuntimeError("not implemented for this class")
 
-    @abstractmethod
-    def is_valid(self, wires: list[np.array], is_partial_list: bool=False) -> bool:
-        """
-        Check the constraint
-        """
-        pass
+    @property
+    def mutates(self) -> bool:
+        return False
 
 class IndicatorConstraint(Constraint):
     """
-    Single indicator tile at a position
+    Meant for wires with a fixed position
     """
-
-    def __init__(self, player_index: int, position_index: int, wire: int):
+    def __init__(self, constraints: list[np.array]):
         super().__init__()
-        self.player_index = player_index
-        self.position_index = position_index
-        self.wire = wire
 
-    def get_positions(self) -> set[tuple[int, int]]:
-        return {(self.player_index, self.position_index)}
-        pass
+        self.constraints = constraints
 
-    def get_wires(self) -> set[int]:
-        return {self.wire}
+    def is_valid(
+            self,
+            wire_rank: int,
+            wire_limits_per_player: np.array,
+            remaining_wires: tuple[int, ...],
+            wire_distribution: tuple[int, ...],
+            is_terminal: bool,
+    ) -> bool:
+        # for each wire we are distributing to the players
+        for player_index, player_wires in enumerate(wire_distribution):
+            for offset in range(player_wires):
+                wire_index = wire_limits_per_player[player_index] - remaining_wires[player_index] + offset
+                constraint_rank = self.constraints[player_index][wire_index]
 
-    def is_valid(self, wires: list[np.array], is_partial_list: bool=False) -> bool:
-        return wires[self.player_index][self.position_index] == self.wire
+                if constraint_rank != self.EMPTY and constraint_rank != wire_rank:
+                    return False
+        return True
 
 class SubsetConstraint(Constraint):
     """
-    Constraint that a subset of wires exists. For example 2/3 yellow wires
+    Meant to represent yellow/red wires
     """
-
     def __init__(self, wires: list[int], num_wires: int):
-        """
-        Constructor
-
-        :param wires: wires that we care about
-        :param num_wires: the maximum number of wires that can exist from the subset
-        """
         super().__init__()
-        self.wires = set(wires)
+        self.wires = wires
         self.num_wires = num_wires
 
-    def get_positions(self) -> set[tuple[int, int]]:
-        return set()
+    def is_valid(
+        self,
+        wire_rank: int,
+        wire_limits_per_player: np.array,
+        remaining_wires: tuple[int, ...],
+        wire_distribution: tuple[int, ...],
+        is_terminal: bool,
+    ) -> bool:
+        # we're at the end, should have zero wires left
+        if is_terminal:
+            return self.num_wires == 0
+        # we don't care, not an important wire
+        elif wire_rank not in self.wires:
+            return True
 
-    def get_wires(self) -> set[int]:
-        return self.wires
+        # check the sum
+        wire_sum = sum(wire_distribution)
+        if wire_sum not in {0, 1}:
+            raise RuntimeError("wires sum should be 0/1")
 
-    def is_valid(self, wires: list[np.array], is_partial_list: bool=False) -> bool:
-        wire_set = set(np.concat(wires))
-        if is_partial_list:
-            return len(wire_set.intersection(self.wires)) < self.num_wires
-        return len(wire_set.intersection(self.wires)) == self.num_wires
+        # no wire to distribute, return whether we have space left
+        if wire_sum == 0:
+            return len(self.wires) < self.num_wires
+        else:
+            # do we have space left
+            return self.num_wires > 0
 
+    def mutate_constraint(
+            self,
+            wire_rank: int,
+            wire_limits_per_player: np.array,
+            remaining_wires: tuple[int, ...],
+            wire_distribution: tuple[int, ...],
+            is_terminal: bool,
+    ):
+        if is_terminal:
+            raise RuntimeError("not implemented for this class")
+        # we don't care, not an important wire
+        elif wire_rank not in self.wires:
+            return self
 
+        wire_sum = sum(wire_distribution)
+        if wire_sum == 0:
+            return self
 
+        remaining_wires = [el for el in self.wires if el != wire_rank]
+        return SubsetConstraint(remaining_wires, self.num_wires - 1)
 
+    def __eq__(self, other):
+        return (
+                sorted(self.wires) == sorted(other.wires)
+                and self.num_wires == other.num_wires
+        )
+
+    def __hash__(self):
+        return hash(('SubsetConstraint', tuple(sorted(self.wires)), self.num_wires))
--- src/game_state_possibilities.py
+++ src/game_state_possibilities.py
@@ -1,9 +1,14 @@
 import time
 from dataclasses import dataclass
+from typing import Optional
+
 import numpy as np
 import functools
 import itertools
 from collections import Counter
+
+from src.constraints import Constraint, IndicatorConstraint, SubsetConstraint
+
 
 @dataclass
 class Player:
@@ -42,6 +47,9 @@ def get_single_wire_rank_distributions(num_wires: int, num_players: int) -> Coun
     :param num_wires: number of wires
     :param num_players: number of players
     """
+    if num_wires == 0:
+        return Counter([tuple([0 for _ in range(num_players)])])
+
     counts = list()
     for combo in itertools.product(range(num_players), repeat=num_wires):
         count = Counter(list(combo))
@@ -51,7 +59,8 @@ def get_single_wire_rank_distributions(num_wires: int, num_players: int) -> Coun
 
 def compute_probability_matrices(
     wire_limits_per_player: np.array,
-    wire_limits: dict[int, int],
+    wire_limits: dict[int, tuple[int, int]],
+    constraints: Optional[list[Constraint]] = None,
 ) -> tuple[list[np.array], list[np.array], float, int]:
     """
     way to recursively compute wire limits
@@ -60,18 +69,25 @@ def compute_probability_matrices(
     Args:
         :param player_index: index of the player
         :param wire_limits_per_player: number of wires you can place per player (not self)
-        :param wire_limits: dictionary of wire limits for the sum of the remaining players
+        :param wire_limits: dictionary of wire limits [min, max] for the sum of the remaining players
+        :param constraints: additional constraints for the hand
     """
     # express this as an array for future jit
     num_players = len(wire_limits_per_player)
     wire_array, wire_array_limits = list(zip(*sorted(wire_limits.items(), key=lambda x: x[0])))
     wire_array = np.array(wire_array, np.int8)
-    wire_array_limits = np.array(wire_array_limits, np.float64)
+    lower_wire_array_limits = np.array([el[0] for el in wire_array_limits], np.float64)
+    upper_wire_array_limits = np.array([el[1] for el in wire_array_limits], np.float64)
+
+    constraints = constraints or []
+    fixed_constraints = [el for el in constraints if not el.mutates]
+    mutating_constraints = tuple([el for el in constraints if el.mutates])
 
     @functools.cache
     def compute_probability_matrices_helper(
         remaining_wires: tuple[int, ...],
-        wire_array_index: int
+        wire_array_index: int,
+        cur_mutating_constraints: tuple[Constraint],
     ) -> tuple[list[np.array], list[np.array], float, int]:
         """
         Helper function that computes all the cached values.
@@ -80,6 +96,7 @@ def compute_probability_matrices(
         Args:
             :param remaining_wires: number of wires for each remaining player
             :param wire_array_index: int in [0, len(wire_array-1)). Which wire to use
+            :param cur_mutating_constraints: additional constraints for the hands
 
         Returns:
             tuple of:
@@ -101,46 +118,78 @@ def compute_probability_matrices(
 
         # we reached the end of wires
         if wire_array_index == len(wire_array):
-            return wire_density_matrices, wire_count_matrices, 1.0, 1
+            constraint_args = [
+                Constraint.EMPTY,
+                wire_limits_per_player,
+                remaining_wires,
+                [0 for _ in range(num_players)],
+                True,
+            ]
+            is_valid = all([constraint.is_valid(*constraint_args) for constraint in cur_mutating_constraints])
+            weight = 1.0 if is_valid else 0.0
+            combinations = 1 if is_valid else 0
+            return wire_density_matrices, wire_count_matrices, weight, combinations
 
-        wire_distributions = get_single_wire_rank_distributions(
-            num_wires=int(wire_array_limits[wire_array_index]),
-            num_players=len(remaining_wires)
-        )
-
-        for wire_distribution, count in wire_distributions.items():
-            # if there are any cases where we have too many wires to distribute towards a player
-            if np.any(np.array(remaining_wires) < np.array(wire_distribution)):
-                continue
-
-            # TODO: add constraints.py checks here once we have them
-            # recursive case, compute sub cases
-            sub_results = compute_probability_matrices_helper(
-                remaining_wires = tuple(np.array(remaining_wires) - np.array(wire_distribution)),
-                wire_array_index=wire_array_index + 1
+        lower_wires = int(lower_wire_array_limits[wire_array_index])
+        upper_wires = int(upper_wire_array_limits[wire_array_index])
+        for num_wires_to_distribute in range(lower_wires, upper_wires + 1):
+            wire_distributions = get_single_wire_rank_distributions(
+                num_wires=int(num_wires_to_distribute),
+                num_players=len(remaining_wires)
             )
 
-            # if the sub results are impossible, continue
-            if sub_results[2] == 0:
-                continue
+            for wire_distribution, count in wire_distributions.items():
+                # if there are any cases where we have too many wires to distribute towards a player
+                if np.any(np.array(remaining_wires) < np.array(wire_distribution)):
+                    continue
 
-            cur_weight = count * sub_results[2]
-            weight += cur_weight
-            combinations += sub_results[3]
+                # arguments to evaluate every constraint
+                constraint_args = [
+                    wire_array[wire_array_index],
+                    wire_limits_per_player,
+                    remaining_wires,
+                    wire_distribution,
+                    False,
+                ]
+                # if a constraint is violated, continue
+                all_constraints = fixed_constraints + list(cur_mutating_constraints)
+                if any(not constraint.is_valid(*constraint_args) for constraint in all_constraints):
+                    continue
 
-            for index, wires_for_player in enumerate(wire_distribution):
-                if wire_array_index != len(wire_array_limits) - 1:
-                    wire_density_matrices[index][wires_for_player:] += sub_results[0][index] * count
-                    wire_count_matrices[index] += sub_results[1][index] * count
-                if wires_for_player > 0:
-                    wire_density_matrices[index][:wires_for_player, wire_array_index] += cur_weight
-                    wire_count_matrices[index][wire_array_index, wires_for_player - 1] += cur_weight
+                # construct the mutating constraints
+                recursive_constraints = []
+                for constraint in cur_mutating_constraints:
+                    recursive_constraints.append(constraint.mutate_constraint(*constraint_args))
+
+                # recursive case, compute sub cases
+                sub_results = compute_probability_matrices_helper(
+                    remaining_wires = tuple(np.array(remaining_wires) - np.array(wire_distribution)),
+                    wire_array_index=wire_array_index + 1,
+                    cur_mutating_constraints=tuple(recursive_constraints),
+                )
+
+                # if the sub results are impossible, continue
+                if sub_results[2] == 0:
+                    continue
+
+                cur_weight = count * sub_results[2]
+                weight += cur_weight
+                combinations += sub_results[3]
+
+                for index, wires_for_player in enumerate(wire_distribution):
+                    if wire_array_index != len(upper_wire_array_limits) - 1:
+                        wire_density_matrices[index][wires_for_player:] += sub_results[0][index] * count
+                        wire_count_matrices[index] += sub_results[1][index] * count
+                    if wires_for_player > 0:
+                        wire_density_matrices[index][:wires_for_player, wire_array_index] += cur_weight
+                        wire_count_matrices[index][wire_array_index, wires_for_player - 1] += cur_weight
 
         return wire_density_matrices, wire_count_matrices, weight, combinations
 
     helper_results = compute_probability_matrices_helper(
         remaining_wires=tuple(wire_limits_per_player),
-        wire_array_index=0
+        wire_array_index=0,
+        cur_mutating_constraints=tuple(mutating_constraints),
     )
 
     for player_index in range(num_players):
@@ -174,7 +223,7 @@ def main():
     n = 2
     results = compute_probability_matrices(
         wire_limits_per_player=np.array([n for _ in range(4)]),
-        wire_limits={i: 4 for i in range(n)}
+        wire_limits={i: (4, 4) for i in range(n)}
     )
     print(results[0][0])
     print(results[1][0])
@@ -188,7 +237,7 @@ def main():
     n = 10
     results = compute_probability_matrices(
         wire_limits_per_player=np.array([n for _ in range(4)]),
-        wire_limits={i: 4 for i in range(n)}
+        wire_limits={i: (4, 4) for i in range(n)}
     )
     print(results[0][0])
     print(results[1][0])
@@ -201,12 +250,13 @@ def main():
     for attempt in range(10):
         # build the deck, select 10 cards as the dealer
         deck = np.array([i for i in range(12) for _ in range(4)])
-        hand = np.random.choice(deck, size=10, replace=True, p=None)
+        hand = np.random.choice(deck, size=10, replace=False, p=None)
         # compute the wire limits per player and the individual wire limits for the remaining cards
         wire_limits_per_player = np.array([(48 + 4 - i) // 5 for i in range(5)])
         wire_limits = {i: 4 for i in range(12)}
         for wire in hand:
             wire_limits[wire] -= 1
+        wire_limits = {k: (v, v) for k, v in wire_limits.items()}
         # profile things
         results = compute_probability_matrices(
             wire_limits_per_player=wire_limits_per_player[1:],
@@ -219,11 +269,63 @@ def main():
     t0 = time.time()
     _ = compute_probability_matrices(
         wire_limits_per_player=np.array([(48 + 4 - i) // 5 for i in range(5)]),
-        wire_limits={i: 4 for i in range(12)},
+        wire_limits={i: (4, 4) for i in range(12)},
     )
     print(time.time() - t0)
 
+def constraint_tests():
+    wires = 4
 
+    t0 = time.time()
+
+    # compute the wire limits per player and the individual wire limits for the remaining cards
+    wire_limits_per_player = np.array([((wires * 4) + 4 - i) // 5 for i in range(5)])
+    wire_limits = {i: (4, 4) for i in range(wires)}
+
+    constraints = [np.ones(limit) * Constraint.EMPTY for limit in wire_limits_per_player]
+    # have one of each wire for the first player
+    constraints[0] = np.array(list(range(len(constraints[0]))))
+    indicator_constraint = IndicatorConstraint(constraints)
+
+    # profile things
+    results = compute_probability_matrices(
+        wire_limits_per_player=wire_limits_per_player,
+        wire_limits=wire_limits,
+        constraints=[indicator_constraint],
+    )
+
+    if not np.all(np.isclose(results[0][0], np.identity(wires))):
+        print(results[0][0])
+        assert False, "If the first player has one of each wire, then the wire density matrix should be the identity matrix"
+
+    print(time.time() - t0)
+
+def constraint_tests2():
+    wires = 12
+
+    t0 = time.time()
+
+    # compute the wire limits per player and the individual wire limits for the remaining cards
+    wire_limits_per_player = np.array([((wires * 4) + 4 - i) // 5 for i in range(5)])
+    wire_limits = {i: (4, 4) for i in range(wires)}
+
+    for i in range(10):
+        constraints = [np.ones(limit) * Constraint.EMPTY for limit in wire_limits_per_player]
+        # have one of each wire for the first player
+
+        deck = np.array([i for i in range(wires) for _ in range(4)])
+        hand = np.random.choice(deck, size=((wires * 4) + 4) // 5, replace=False, p=None)
+        constraints[0] = np.array(list(sorted(hand)))
+        indicator_constraint = IndicatorConstraint(constraints)
+        # profile things
+        _ = compute_probability_matrices(
+            wire_limits_per_player=wire_limits_per_player,
+            wire_limits=wire_limits,
+            constraints=[indicator_constraint],
+        )
+        print(i, time.time() - t0)
+
+    print(time.time() - t0)
 
 if __name__ == '__main__':
-    main()
+    constraint_tests2()
